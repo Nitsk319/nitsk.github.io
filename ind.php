4f3b7da9b787d7888ec39b314a1d5c235d3aac0d
Nitsk319 <143601063+Nitsk319@users.noreply.github.com>
2023-09-01 16:03:44 +0530

    Attendance Management System

---
+++ ind.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creative Home Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Admin</a></li>
+                <li><a href="about.php">Employee</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <section class="hero">
+            <div class="hero-content">
+                <h1>Welcome to Attendance Management System</h1>
+                <p>Your journey to inspiration begins here.</p>
+                <a href="#features" class="cta-button">Explore</a>
+            </div>
+        </section>
+
+        <!--<section id="features" class="features">
+            <div class="feature">
+                <h2>Feature 1</h2>
+                <p>Discover something amazing.</p>
+            </div>
+            <div class="feature">
+                <h2>Feature 2</h2>
+                <p>Experience the extraordinary.</p>
+            </div>
+            <div class="feature">
+                <h2>Feature 3</h2>
+                <p>Unleash your creativity.</p>
+            </div>
+        </section>-->
+    </main>
+    
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?>All rights reserved.</p>
+    </footer>
+    
+    <script src="script.js"></script>
+</body>
+</html>
